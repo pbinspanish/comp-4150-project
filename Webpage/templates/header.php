a1f8c607456f10849151ca9fb654508ee2c91e5c
Josiah Henson <pbinspanish@outlook.com>
2024-11-20 14:26:03 -0500

    update bootstrap and remove uneeded navigation items

--- Webpage/templates/header.php
+++ Webpage/templates/header.php
@@ -4,8 +4,8 @@
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>Employee Database</title>
-		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 	</head>
 	<body>
 		<nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
@@ -15,7 +15,7 @@
 					<span class="navbar-toggler-icon"></span>
 				</button>
 				<div class="collapse navbar-collapse" id="navbarNav">
-					<ul class="navbar-nav">
+					<!-- <ul class="navbar-nav">
 						<li class="nav-item">
 							<a class="nav-link" href="index.php">Home</a>
 						</li>
@@ -28,7 +28,7 @@
 						<li class="nav-item">
 							<a class="nav-link" href="department_summary.php">Department Summary</a>
 						</li>
-					</ul>
+					</ul> -->
 				</div>
 			</div>
 		</nav>
