474adede3f5123afe300feb2f10e249aeb03b443
Josiah Henson <pbinspanish@outlook.com>
2024-11-20 15:30:25 -0500

    updating spacing in index

--- Webpage/index.php
+++ Webpage/index.php
@@ -3,19 +3,19 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/lib/db_config.php';
 include $_SERVER['DOCUMENT_ROOT'] . '/templates/header.php';
 ?>
 
-<div class="row justify-content-center">
+<div class="row">
 	<div class="col-auto">
 		<h1>Employee Database</h1>
 	</div>
 </div>
 
-<div class="row mt-4 justify-content-center">
+<div class="row mt-4">
 	<div class="col-auto">
 		<a class="btn btn-primary" href="/employee/employee_login.php" role="button">Employee Login</a>
 	</div>
 </div>
 
-<div class="row mt-3 justify-content-center">
+<div class="row mt-3">
 	<div class="col-auto">
 		<a class="btn btn-primary" href="/manager/manager_login.php" role="button">Manager Login</a>
 	</div>
