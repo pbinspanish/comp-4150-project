9dba160c639a3aefa88915a383ca3b88f744a2a0
Josiah Henson <pbinspanish@outlook.com>
2024-11-20 14:25:50 -0500

    add template to empty files

---
+++ Webpage/employee/employee_login.php
@@ -0,0 +1,10 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/lib/db_config.php';
+include $_SERVER['DOCUMENT_ROOT'] . '/templates/header.php';
+?>
+
+Employee Login
+
+<?php
+include $_SERVER['DOCUMENT_ROOT'] . '/templates/footer.php';
+?>
